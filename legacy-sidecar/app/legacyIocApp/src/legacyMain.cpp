d1a7aaf1217e7fa1c7ed32390b7bbfed0ccf844a
Derek Steinkamp <derekste@fnal.gov>
2026-05-12 17:00:30 -0500

    Keep legacy sidecar alive without TTY

--- legacy-sidecar/app/legacyIocApp/src/legacyMain.cpp
+++ legacy-sidecar/app/legacyIocApp/src/legacyMain.cpp
@@ -1,8 +1,21 @@
 #include <stddef.h>
+#include <signal.h>
+#include <unistd.h>
 
 #include "epicsExit.h"
 #include "epicsThread.h"
 #include "iocsh.h"
+
+namespace {
+
+volatile sig_atomic_t shutdownRequested = 0;
+
+void requestShutdown(int)
+{
+    shutdownRequested = 1;
+}
+
+} // namespace
 
 int main(int argc, char* argv[])
 {
@@ -10,7 +23,18 @@ int main(int argc, char* argv[])
         iocsh(argv[1]);
         epicsThreadSleep(.2);
     }
-    iocsh(nullptr);
+
+    if (isatty(STDIN_FILENO)) {
+        iocsh(nullptr);
+    } else {
+        signal(SIGTERM, requestShutdown);
+        signal(SIGINT, requestShutdown);
+
+        while (!shutdownRequested) {
+            epicsThreadSleep(1.0);
+        }
+    }
+
     epicsExit(0);
     return 0;
 }
